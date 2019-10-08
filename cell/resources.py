63764207e2784227943a134abf69f5bc9b009c3a
ezrankayamba <ezrankayamba@gmail.com>
2019-10-08 22:11:02 +0300

    Phone no import fix

--- cell/resources.py
+++ cell/resources.py
@@ -1,7 +1,16 @@
 from import_export import resources
 from .models import Member
+import re
 
 
 class MemberResource(resources.ModelResource):
+    regex = '(\d{9})\.0$'
+
     class Meta:
         model = Member
+
+    def before_save_instance(self, instance, using_transactions, dry_run):
+        print(instance.phone_no)
+        instance.phone_no = f'255{re.findall(self.regex, str(instance.phone_no))[0]}'
+        print(instance.phone_no)
+        return instance
